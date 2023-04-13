15ab904fd3c08da8291b9d3c8569980ba4c28d5b
Sean Fulton <seanfulton790@gmail.com>
2023-04-13 13:44:20 +0100

    change DEBUG check in tasks.py to string

--- pressfeed/news/apps.py
+++ pressfeed/news/apps.py
@@ -8,7 +8,7 @@ class NewsConfig(AppConfig):
 
     # I have created this ready function to ensure my scheduler from news/tasks.py is started when the news app has
     def ready(self):
-        if (os.environ.get('RUN_MAIN') or os.environ.get('DEBUG') == False):
+        if (os.environ.get('RUN_MAIN') or os.environ.get('DEBUG') == 'False'):
             from .tasks import start_scheduler
             start_scheduler()
 
