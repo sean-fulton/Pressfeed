0adbd9e075555c1fde5988994e2a17780ae4dc22
Sean Fulton <seanfulton790@gmail.com>
2023-04-13 14:25:33 +0100

    fix ready executing for news app

--- pressfeed/news/apps.py
+++ pressfeed/news/apps.py
@@ -8,7 +8,7 @@ class NewsConfig(AppConfig):
 
     # I have created this ready function to ensure my scheduler from news/tasks.py is started when the news app has
     def ready(self):
-        if (os.environ.get('RUN_MAIN')):
+        if (os.environ.get('RUN_MAIN') == 'true'):
             from .tasks import start_scheduler
             start_scheduler()
 
