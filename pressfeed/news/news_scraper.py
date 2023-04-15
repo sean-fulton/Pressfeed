71f5d686f12e8d6dede63207867cd727ffc26a81
Sean Fulton <seanfulton790@gmail.com>
2023-04-15 21:57:36 +0100

    ensure store_news execute if news_data is none

--- pressfeed/news/news_scraper.py
+++ pressfeed/news/news_scraper.py
@@ -9,6 +9,7 @@ from django.db.utils import IntegrityError
 env = environ.Env()
 environ.Env.read_env()
 
+
 # Simple method to encapsulate the logic of both retieve news and store news to be called by the scheduler / managment command
 def update_news():
     irish_news_data = retrieve_news('ie')
@@ -24,6 +25,7 @@ def update_news():
     formatted_date = now.strftime("%Y-%m-%d %H:%M:%S")
     print("Updated News at " + formatted_date)
 
+
 # Calls NewsAPI top-headlines endpoint to scrape news data based on the country param
 def retrieve_news(country):
     response = requests.get(f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={env("NEWSAPI_KEY")}')
@@ -31,25 +33,28 @@ def retrieve_news(country):
     if response.status_code == 200:
         return response.json()
 
+
 # Persists news data from a json object to the article and source models
 def store_news(news_data):
-    for article in news_data['articles']:
-        if (article['title'] is not None) and (article['description'] is not None) and (article['url'] is not None):
-            try:
+    if news_data is not None and 'articles' in news_data:
+        for article in news_data['articles']:
+            if (article['title'] is not None) and (article['description'] is not None) and (article['url'] is not None):
+                try:
 
-                source, created = Source.objects.get_or_create(
-                    name=article['source']['name']
-                )
+                    source, created = Source.objects.get_or_create(
+                        name=article['source']['name']
+                    )
 
-                article_obj, created = Article.objects.get_or_create(
-                    title=article['title'],
-                    description=article['description'],
-                    url=article['url'],
-                    published_at=article['publishedAt'],
-                    source=source,
-                    thumbnail_url=article['urlToImage']
-                )
-            except IntegrityError:
-                # Ignore duplicate article entries
-                pass
-
+                    article_obj, created = Article.objects.get_or_create(
+                        title=article['title'],
+                        description=article['description'],
+                        url=article['url'],
+                        published_at=article['publishedAt'],
+                        source=source,
+                        thumbnail_url=article['urlToImage']
+                    )
+                except IntegrityError:
+                    # Ignore duplicate article entries
+                    pass
+    else:
+        return
