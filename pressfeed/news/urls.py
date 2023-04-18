f2297e0423b3f285b2987f8b05b1e479e4c06849
Sean Fulton <seanfulton790@gmail.com>
2023-04-18 12:14:30 +0100

    remove accessibility to the test newsfeed

--- pressfeed/news/urls.py
+++ pressfeed/news/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('test/', views.testfeed, name="testfeed"),
+    # path('test/', views.testfeed, name="testfeed"),
     path('feed/', views.newsfeed, name="newsfeed"),
     path('subscribe/', views.subscribe, name="subscribe")
 ]
