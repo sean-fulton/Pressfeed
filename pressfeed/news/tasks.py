986fc5753676a9319ff531cca0697389897777d1
Sean Fulton <seanfulton790@gmail.com>
2023-04-20 12:54:46 +0100

    remove thread lock in for update news task

--- pressfeed/news/tasks.py
+++ pressfeed/news/tasks.py
@@ -3,17 +3,16 @@ from . news_scraper import update_news
 import schedule
 import time
 
-lock = threading.Lock()
-
 def start_scheduler():
     def scheduler():
+        #Wait for db migrations to pass before executing
         time.sleep(10)
         # Schedule update_news every 4 hours
         schedule.every(4).hours.do(update_news)
+        #initial call at app startup to update news
         update_news()
         while True:
-            with lock:
-                schedule.run_pending()
+            schedule.run_pending()
             time.sleep(1)
 
     scheduler_thread = threading.Thread(target=scheduler)
