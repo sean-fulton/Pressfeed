c94561d90072c5d1f022a91c861789269adc7acb
Sean Fulton <seanfulton790@gmail.com>
2023-05-08 15:06:36 +0100

    final

--- pressfeed/news/views.py
+++ pressfeed/news/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import Article, Source, Comment, Like, Dislike
-from django.core.paginator import Paginator, EmptyPage
+from django.core.paginator import Paginator
 from pressfeed.forms import CommentForm
 from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
@@ -31,20 +31,6 @@ def subscribe(request):
     # If no form submitted, show user's subscribed sources
     user_sources = request.user.sources.all()
     return render(request, 'subscribe.html', {'sources': sources, 'user_sources': user_sources})
-
-def testfeed(request):
-    # use NewsAPI call to fetch article data
-    # start = time.time()
-    # irish_news_request = requests.get(f'https://newsapi.org/v2/top-headlines?country=ie&apiKey={env("NEWSAPI_KEY")}')
-    # newsapi_response = json.loads(irish_news_request.content)
-    # end = time.time()
-    # print(f'TIME TO CALL API: {end - start}')
-    # return render(request, 'testfeed.html', {'api': newsapi_response})
-
-    # use PostgreSQL Article model to fetch article data
-
-    articles = Article.objects.order_by('-published_at')
-    return render(request, 'testfeed.html', {'articles': articles})
 
 @login_required
 def newsfeed(request):
